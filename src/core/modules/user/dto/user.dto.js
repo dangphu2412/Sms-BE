109b0f79d21ec3cdddf30b939f74d129fd3c8d88
ichhoa <ichhoa129@gmail.com>
2021-04-20 20:19:31 +0700

    [SMS-16] Create basic user information - Add create user swagger

--- src/core/modules/user/dto/user.dto.js
+++ src/core/modules/user/dto/user.dto.js
@@ -1,3 +1,27 @@
+import { SwaggerDocument } from '../../../../packages/swagger';
+import { ApiDocument } from '../../../config/swagger';
+
+ApiDocument.addModel('profile',
+{
+    firstName: SwaggerDocument.ApiProperty({ type: 'String' }),
+    lastName: SwaggerDocument.ApiProperty({ type: 'String' }),
+    birthday: SwaggerDocument.ApiProperty({ type: 'String' }),
+    phone: SwaggerDocument.ApiProperty({ type: 'String' }),
+    hometown: SwaggerDocument.ApiProperty({ type: 'String' }),
+    gender: SwaggerDocument.ApiProperty({ type: 'Boolean' }),
+    facebook: SwaggerDocument.ApiProperty({ type: 'String' }),
+    universityId: SwaggerDocument.ApiProperty({ type: 'String' })
+});
+
+ApiDocument.addModel('CreateDto',
+{
+    email: SwaggerDocument.ApiProperty({ type: 'String' }),
+    password: SwaggerDocument.ApiProperty({ type: 'String' }),
+    fingerprint: SwaggerDocument.ApiProperty({ type: 'String' }),
+    status: SwaggerDocument.ApiProperty({ type: 'String' }),
+    profile: SwaggerDocument.ApiProperty({ model: 'profile' }),
+});
+
 export const CreateDto = body => ({
     email: body.email,
     password: body.password,
@@ -9,8 +33,8 @@ export const CreateDto = body => ({
         birthday: body.profile.birthday,
         phone: body.profile.phone,
         hometown: body.profile.hometown,
-        gender: body.gender,
-        facebook: body.facebook,
+        gender: body.profile.gender,
+        facebook: body.profile.facebook,
         universityId: body.profile.universityId,
     }
 });

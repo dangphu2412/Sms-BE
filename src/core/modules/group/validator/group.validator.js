77f1b0659f77423208a147d62fd836824c57bfaa
Jayjaynoplays <nhenh2712@gmail.com>
2021-04-27 02:23:14 +0700

    [SMS-17]:sparkles: add duplicate validate for req body

--- src/core/modules/group/validator/group.validator.js
+++ src/core/modules/group/validator/group.validator.js
@@ -7,6 +7,7 @@ class Validator {
             name: Joi.string().min(0).required(),
             description: Joi.string().optional(),
 <<<<<<< HEAD
+<<<<<<< HEAD
             childIds: Joi.array().items(Joi.string().regex(/^[0-9a-fA-F]{24}$/)).optional(),
             parentId: Joi.string().regex(/^[0-9a-fA-F]{24}$/).optional(),
             leaderId: Joi.string().regex(/^[0-9a-fA-F]{24}$/).required(),
@@ -17,6 +18,12 @@ class Validator {
             leaderId: Joi.string().regex(/^[0-9a-fA-F]{24}$/).required(),
             userIds: Joi.array().regex(/^[0-9a-fA-F]{24}$/).items(Joi.string()).optional()
 >>>>>>> 6ff6bd0 ([SMS-17]:hammer: renaming the query helper, add more validate option)
+=======
+            childIds: Joi.array().items(Joi.string().regex(/^[0-9a-fA-F]{24}$/)).unique().optional(),
+            parentId: Joi.string().regex(/^[0-9a-fA-F]{24}$/).optional(),
+            leaderId: Joi.string().regex(/^[0-9a-fA-F]{24}$/).required(),
+            userIds: Joi.array().items(Joi.string().regex(/^[0-9a-fA-F]{24}$/)).unique().optional()
+>>>>>>> b0ae0f0 ([SMS-17]:sparkles: add duplicate validate for req body)
         });
         return joiFilter(schema, 'body');
     }

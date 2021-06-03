b7efa4de6b18880ae9632f8017d8a1b1feae1c8f
ichhoa <ichhoa129@gmail.com>
2021-06-03 17:45:33 +0700

    Validate datetime in excel module
    - Add dayjs package

--- src/core/utils/timeConvert.js
+++ src/core/utils/timeConvert.js
@@ -1,8 +1,12 @@
-const DATE_PATTERN = '^(?:(?:31(\\/|-|\\.)(?:0?[13578]|1[02]))\\1|(?:(?:29|30)(\\/|-|\\.)(?:0?[13-9]|1[0-2])\\2))(?:(?:1[6-9]|[2-9]\\d)?\\d{2})$|^(?:29(\\/|-|\\.)0?2\\3(?:(?:(?:1[6-9]|[2-9]\\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\\d|2[0-8])(\\/|-|\\.)(?:(?:0?[1-9])|(?:1[0-2]))\\4(?:(?:1[6-9]|[2-9]\\d)?\\d{2})$';
-const regex = new RegExp(DATE_PATTERN, 'g');
+import dayjs from 'dayjs';
+import customParseFormat from 'dayjs/plugin/customParseFormat';
+
+dayjs.extend(customParseFormat);
+const DATE_FORMAT = 'DD/MM/YYYY';
+
 export const toTimestamp = strDate => {
-    // validate date
-    if (!strDate.match(regex)) return NaN;
+    if (!strDate || strDate === '') return null;
+    if (!dayjs(strDate, DATE_FORMAT, true).isValid()) return NaN;
 
     const splDate = strDate.split('/');
     const date = new Date(splDate[2], splDate[1] - 1, splDate[0]);

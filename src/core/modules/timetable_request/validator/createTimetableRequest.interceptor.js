a05276f339152a835d30991460a28f3064d004f5
H Nguyen Jayden <nhenh2712@gmail.com>
2021-07-02 16:42:39 +0700

    [SMS-100]enhance form modules

--- src/core/modules/timetable_request/validator/createTimetableRequest.interceptor.js
+++ src/core/modules/timetable_request/validator/createTimetableRequest.interceptor.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { responseJoiError } from 'core/utils';
 import { CreateTimetableRequestSchema } from './createTimetableRequest.schema';
 
@@ -10,4 +11,30 @@ export class CreateTimetableRequestInterceptor {
         }
         return next();
     }
+=======
+/* eslint-disable max-len */
+import { BaseValidateInterceptor } from 'core/infrastructure/interceptor';
+import { SchemaValidatorBuilder } from 'core/utils';
+import { TIMETABLE_REQUEST_TYPE } from 'core/common/enum/timetableRequest.enum';
+import Joi from 'joi';
+
+export class CreateTimetableRequestInterceptor extends BaseValidateInterceptor {
+    getSchema = () => Joi.object().keys({
+        userId: SchemaValidatorBuilder.getIdObjectBuilder().required(),
+        type: Joi.string().valid(TIMETABLE_REQUEST_TYPE.OUT, TIMETABLE_REQUEST_TYPE.ABSENT_ADD, TIMETABLE_REQUEST_TYPE.ABSENT, TIMETABLE_REQUEST_TYPE.LATE, TIMETABLE_REQUEST_TYPE.SOON, TIMETABLE_REQUEST_TYPE.ADD).required(),
+        description: Joi.string().required(),
+        attachment: SchemaValidatorBuilder.getOptionalStringBuilder().optional(),
+        tempTimetables: Joi.array().items(Joi.object().keys({
+            userId: SchemaValidatorBuilder.getIdObjectBuilder().required(),
+            groupId: SchemaValidatorBuilder.getIdObjectBuilder().optional(),
+            timetableId: SchemaValidatorBuilder.getIdObjectBuilder().required(),
+            registerTime: SchemaValidatorBuilder.getIdObjectBuilder().optional(),
+            appliedDate: SchemaValidatorBuilder.getDateBuilder().required(),
+            type: Joi.string().valid(TIMETABLE_REQUEST_TYPE.OUT, TIMETABLE_REQUEST_TYPE.ABSENT_ADD, TIMETABLE_REQUEST_TYPE.ABSENT, TIMETABLE_REQUEST_TYPE.LATE, TIMETABLE_REQUEST_TYPE.SOON, TIMETABLE_REQUEST_TYPE.ADD).required(),
+            customStartTime: Joi.string().optional(),
+            customEndTime: Joi.string().optional()
+        })),
+        isApproved: Joi.boolean().valid(false).optional(),
+    })
+>>>>>>> b1b8809 ([SMS-100]enhance form modules)
 }
